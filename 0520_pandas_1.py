154b791963ec4961a7f2963d3a9ae694a6384db7
Ansxter <GAnnLiu95@gmail.com>
2026-05-20 16:37:39 +0800

    test

--- 0520_pandas_1.py
+++ 0520_pandas_1.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import numpy as np
 
-# 建立 list
+
 stock1 = pd.Series([120, 80, None, 60, 95, None, 110])
 
-# 加入索引建立 stock2
+
 fruits = ["Apple", "Banana", "Orange", "Mango", "Grape", "Peach", "Melon"]
 stock2 = pd.Series([120, 80, None, 60, 95, None, 110], index=fruits)
 
-# 轉為字典
+
 stock3 = stock2.to_dict()
 
-# 輸出結果
+
 print("stock1")
 print(stock1)
 
@@ -21,16 +21,16 @@ print(stock2)
 print("\nstock3")
 print(stock3)
 
-# Banana 庫存值
+
 print("\nBanana 庫存：", stock2["Banana"])
 
-# 缺失值檢查
+
 print("\n缺失值檢查：")
 print(stock2.isnull())
 
-# 缺失值數量
+
 print("\n缺失值數量：", stock2.isnull().sum())
 
-# 存成 CSV 檔
+
 stock2.to_csv("0520_stock.csv", header=["Stock"])
 
